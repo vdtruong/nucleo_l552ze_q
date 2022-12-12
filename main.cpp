426b9412ee43f8c9eb2cd07602fb751f6bb952d2
vdtruong <vandtruong@hotmail.com>
2022-12-11 22:01:55 -0800

    [take_dat_ptr_out] Got gps signal to go through rx.  This signal repeats by itself. Also, got the pc uart speed to debug at 115,200 baud rate.

--- main.cpp
+++ main.cpp
@@ -7,10 +7,13 @@
 //#include <iic_sensors.cpp>
 #include <stdio.h>
 #include <errno.h>
+// Make sure you add sd card in mbed_app.json file or sd card won't work.
+// If you don't have mbed_app_json, add this file.
 // Blockdevice.
 #include "mbed-os/storage/blockdevice/COMPONENT_SD/include/SD/SDBlockDevice.h"
 // File system.
 #include "FATFileSystem.h"
+#include "BufferedSerial.h"
 
 // Blinking rate in milliseconds
 #define BLINKING_RATE   500ms
@@ -19,12 +22,17 @@
 //#define PRINTF_RATE     30000ms // 30 seconds.
 //#define PRINTF_RATE     15000ms // 15 seconds.
 //#define PRINTF_RATE     10000ms // 10 seconds.
-#define PRINTF_RATE     5000ms // 10 seconds.
+#define PRINTF_RATE     5000ms // 5 seconds.
+#define ONE_SEC         1000ms // 1 seconds.
+#define TWO_SEC         2000ms // 2 seconds.
+#define FIVE_SEC        5000ms // 1 seconds.
+
+//BufferedSerial pc(USBTX, USBRX, 9600);
 
 // Structure to hold shtc3 data.
 struct Shtc3Outputs
 {
-	char data[19];  // Sensors data. 
+	char data[19];  // Sensors data. Shtc3 actually only needs 6 bytes.
 	//char *data;     // Sensors data array. 
 	char done;		// State machine is done. 
 };
@@ -34,12 +42,17 @@ struct Shtc3Outputs i2c_fsm_shtc3(void);        // Function returns the structur
 struct Shtc3Outputs *i2c_fsm_shtc3_ptr(void);   // Function returns the pointer to structure. 
 struct Shtc3Outputs *i2c_d6t_ptr(void);         // Function returns the pointer to structure. 
 struct Shtc3Outputs *i2c_d6t_8l_ptr(void);      // Function returns the pointer to structure. 
-//char *i2c_d6t_8l_ptr(void);                     // Function returns the pointer to char. 
+char *i2c_d6t_8L_ptr(void);                     // Function returns the pointer to char. 
+void ser_com (void);                            // Serial function to get data from gps.
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Create an I2C object.
 //I2C i2c(I2C_SDA , I2C_SCL);
-
+//I2C i2c(ARDUINO_UNO_D14, ARDUINO_UNO_D15);  // i2c1_sda, i2c1_scl
+    
+// Set up the serial for the board.
+//static BufferedSerial get_gps(D8, D7, 9600);
+    
 /***** Function begins *****************************************************************************************/
 // This is for the Sensirion shtc3 sensor. Working.
 struct Shtc3Outputs i2c_fsm_shtc3(void)
@@ -99,6 +112,7 @@ struct Shtc3Outputs i2c_fsm_shtc3(void)
     
     return ste_out;
 }
+
 // This is for the Sensirion shtc3 sensor. 
 struct Shtc3Outputs *i2c_fsm_shtc3_ptr(void)
 {
@@ -169,6 +183,7 @@ struct Shtc3Outputs *i2c_fsm_shtc3_ptr(void)
 
     return ste_out_ptr; // Return pointer to sensor outputs.
 }
+
 // This is for the Omron D6T-1A-02 sensor. 
 struct Shtc3Outputs *i2c_d6t_ptr(void)
 {
@@ -234,6 +249,7 @@ struct Shtc3Outputs *i2c_d6t_ptr(void)
         
     return ste_out_ptr; // Return pointer to sensor outputs.
 }
+
 // This is for the Omron D6T-8L sensor. 
 struct Shtc3Outputs *i2c_d6t_8l_ptr(void)   // Function returns pointer to structure.
 //char *i2c_d6t_8l_ptr(void)                   // Function returns pointer to character.
@@ -385,6 +401,125 @@ struct Shtc3Outputs *i2c_d6t_8l_ptr(void)   // Function returns pointer to struc
     return ste_out_ptr; // Return pointer to sensor outputs.
 }
 
+char *i2c_d6t_8L_ptr(void)   // Function returns pointer to char.
+{
+	int d6t_addr = 0x14;                // Omron D6T address.
+	char i2c_buffer[10]= 	{
+								0x14,	// Write address.       0	
+								0x4C,	// Meas. command	    1
+								0x02,	// 	                    2
+								0x05,	// 	                    3 		0
+								0x03,	//  	                4		1
+								0x15,	// Send addr. read		5		2
+								0x00,   // 	                    6		3
+								0x00,	// 	                    7		4
+								0x00,	// 		                8		5
+								0x00 	// 		                9		6
+							};	
+    char bufr_1[4]= 	{
+								0x02,	// 0	
+								0x00,	// 1
+								0x01,	// 2
+								0xee	// 3----
+						};	
+    char bufr_2[4]= 	{
+								0x05,	// 4
+								0x90,	// 5
+								0x3a,   // 6
+								0xb8    // 7----		
+						};	
+    char bufr_3[4]= 	{
+								0x03,	// 8
+								0x00,   // 9
+                                0x03,   // 10
+                                0x8b    // 11----
+                        };	
+    char bufr_4[4]= 	{
+								0x03,   // 12
+                                0x00,   // 13
+                                0x07,   // 14
+                                0x97    // 15----
+                        };	
+    char bufr_5[4]= 	{
+								0x02,   // 16
+                                0x00,   // 17
+                                0x00,   // 18
+                                0xe9 	// 19----
+						};	
+    char sens_dat[19]= 	    {
+                                0x00,   // 0	
+	                            0x00,   // 1
+                                0x00,	// 2
+		                        0x00,	// 3
+	                            0x00,	// 4
+	                            0x00,	// 5
+                                0x00,   // 6	
+	                            0x00,   // 7
+                                0x00,	// 8
+		                        0x00,	// 9
+	                            0x00,	// 10
+	                            0x00,	// 11
+                                0x00,   // 12	
+	                            0x00,   // 13
+                                0x00,	// 14
+		                        0x00,	// 15
+	                            0x00,	// 16
+	                            0x00,	// 17
+                                0x00    // 18                            
+                            };
+	
+    // Create an I2C object.
+    // Using arduino pins for sda and scl.
+    I2C i2c(ARDUINO_UNO_D14, ARDUINO_UNO_D15);  // i2c1_sda, i2c1_scl
+    
+    /* To get the data from sensor do next several steps. */
+    // Step 1.
+    i2c.start();
+    i2c.write(d6t_addr, bufr_1 + 0, 4);
+    i2c.stop();
+    // Step 2.
+    i2c.start();
+    i2c.write(d6t_addr, bufr_2 + 0, 4);
+    i2c.stop();
+    // Step 3.
+    i2c.start();
+    i2c.write(d6t_addr, bufr_3 + 0, 4);
+    i2c.stop();
+    // Step 4.
+    i2c.start();
+    i2c.write(d6t_addr, bufr_4 + 0, 4);
+    i2c.stop();
+    // Step 5.
+    i2c.start();
+    i2c.write(d6t_addr, bufr_5 + 0, 4);
+    i2c.stop();
+    ThisThread::sleep_for(250ms);
+    // Step 6.
+    i2c.start();
+    i2c.write(d6t_addr, i2c_buffer + 1, 1, true);
+    // Step 7.
+    i2c.read(d6t_addr, sens_dat, 19);   // This works.
+    //i2c.read(d6t_addr, ste_out_ptr->data, 19);  // Not working.
+    i2c.stop();
+    
+    ThisThread::sleep_for(2ms);
+
+    // This works but you will get a warning.
+    return sens_dat; // Return pointer to sensor outputs.
+}
+
+// Serial function to get data from gps.
+void ser_com (void)
+{
+    static BufferedSerial get_gps(PD_8, PD_9);  // working
+    char *gps_dat = new char[1];
+
+    if (get_gps.readable()) {
+        get_gps.read(gps_dat, sizeof(gps_dat));
+    }
+
+    printf("Gps char = %c", *(gps_dat+0));
+}
 // Physical block device, can be any device that supports the BlockDevice API
 SDBlockDevice   blockDevice(ARDUINO_UNO_D11, ARDUINO_UNO_D12, ARDUINO_UNO_D13, ARDUINO_UNO_D4);  // mosi, miso, sck, cs
 //SDBlockDevice   blockDevice(ARDUINO_UNO_SPI_MOSI, ARDUINO_UNO_SPI_MISO, ARDUINO_UNO_SPI_SCK, ARDUINO_UNO_SPI_CS);  // mosi, miso, sck, cs
@@ -402,6 +537,9 @@ int main()
     struct Shtc3Outputs sen_dat;
     // Pointer to get shtc3 sensor data.
     struct Shtc3Outputs *sen_dat_ptr;
+    // Pointer to get d6t_8L sensor data.
+    char *d6t_8L_dat;
+
     int err = 0;
     
     set_time(1256729737);  // Set RTC time to Wed, 28 Oct 2009 11:35:37
@@ -475,8 +613,8 @@ int main()
 
         led = !led;
         //ThisThread::sleep_for(BLINKING_RATE);
-        ThisThread::sleep_for(PRINTF_RATE);
-        printf("Wait every 5 seconds... ");
+        ThisThread::sleep_for(TWO_SEC);
+        printf("Wait every 2 seconds... ");
         
         // This works.
         //sen_dat = i2c_fsm_shtc3();
@@ -492,9 +630,26 @@ int main()
         
         // For D6t-8L sensor.
         //sen_dat_ptr = i2c_d6t_8l_ptr();
-        i2c_d6t_8l_ptr();
+        //i2c_d6t_8l_ptr();
+        d6t_8L_dat = i2c_d6t_8L_ptr();
+        printf("8L_data = %c,%c %c,%c %c,%c %c,%c %c,%c %c,%c %c,%c %c,%c %c,%c",  
+                                                            *(d6t_8L_dat + 0), *(d6t_8L_dat + 1),
+                                                            *(d6t_8L_dat + 2), *(d6t_8L_dat + 3),
+                                                            *(d6t_8L_dat + 4), *(d6t_8L_dat + 5),
+                                                            *(d6t_8L_dat + 6), *(d6t_8L_dat + 7),
+                                                            *(d6t_8L_dat + 8), *(d6t_8L_dat + 9),
+                                                            *(d6t_8L_dat + 10), *(d6t_8L_dat + 11),
+                                                            *(d6t_8L_dat + 12), *(d6t_8L_dat + 13),
+                                                            *(d6t_8L_dat + 14), *(d6t_8L_dat + 15),
+                                                            *(d6t_8L_dat + 16), *(d6t_8L_dat + 17)
+                                                            );
         //printf("First data element = %s ", (sen_dat_ptr->data)+1);    // okay
         //printf("First data element = %s ", sen_dat_ptr->data+1);      // okay
         //printf("Third char = %s ", (sen_dat_ptr->data)+1);              // okay
+        
+        ThisThread::sleep_for(10ms);
+        
+        ser_com();
+
     }
 }
